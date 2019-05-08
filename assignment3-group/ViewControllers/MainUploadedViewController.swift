a2034c2bad3865fdf2aa979f81e57c9fe6a9883c
Tri Vang <s3726101@rmit.edu.vn>
2019-05-08 18:18:56 +0700

    Update 0.13
    Update camera photo, video (take + upload)

--- assignment3-group/ViewControllers/MainUploadedViewController.swift
+++ assignment3-group/ViewControllers/MainUploadedViewController.swift
@@ -7,25 +7,191 @@
 //
 
 import UIKit
-
+import Firebase
+import Photos
+import MobileCoreServices
 //Upload images, video to firebase and store in model
-class MainUploadedViewController: UIViewController {
-
+class MainUploadedViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var imagePicker = UIImagePickerController()
     override func viewDidLoad() {
+        ref2 = Database.database().reference()
         super.viewDidLoad()
-
+        self.display2()
+        
         // Do any additional setup after loading the view.
     }
-    
+    var choice = 0
+    var ref2: DatabaseReference?
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var imageView: UIImageView!
+    func takePhoto() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            
+            imagePicker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+            print("Here: \(String(describing: imageView))")
+        }
+        
     }
-    */
-
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if(choice == 1){
+            print("here")
+            imagePicker.dismiss(animated: true, completion: nil)
+            imageView.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            uploadPhoto()
+        }
+        if(choice == 2){
+            print("wawawa")
+            dismiss(animated: true, completion: nil)
+            guard let mediaType = info[UIImagePickerController.InfoKey.mediaType] as? String,
+                mediaType == (kUTTypeMovie as String),
+                let url = info[UIImagePickerController.InfoKey.mediaURL] as? URL,
+                UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(url.path)
+                else { return }
+            uploadVideo(url1: url )
+            // Handle a movie capture
+            UISaveVideoAtPathToSavedPhotosAlbum(url.path, self, #selector(video(_:didFinishSavingWithError:contextInfo:)), nil)
+        }
+    }
+    func uploadVideo(url1: URL){
+        let meta = StorageMetadata()
+        meta.contentType = "video"
+        //Get user's to create or access user's folder to store images
+        //                let userID = Auth.auth().currentUser!.uid
+        let tempUserID = "user01"
+        let fileref = Storage.storage().reference().child("/test")
+        //        let image = imageView.image!
+        //get the PNG data for this image
+        //        let data = image.pngData()
+        //        let videoData = try Data(contentsOf: url   as URL)
+        do {
+            let videoData = try Data(contentsOf: url1    as URL)
+            fileref.putData(videoData, metadata: meta, completion: { (meta, error) in
+                if error == nil {
+                    self.ref2?.child("Usershaha/account2/image/test2").setValue("filename")
+                    print("updated")
+                }
+            })
+        } catch {
+            print("unable to convert url to data")
+        }
+        
+    }
+    func uploadPhoto(){
+        let gameKey = ref2?.child("Usershaha/account2").key
+        let filename = "\(gameKey).png"
+        let meta = StorageMetadata()
+        meta.contentType = "image/png"
+        //Get user's to create or access user's folder to store images
+        //                let userID = Auth.auth().currentUser!.uid
+        let tempUserID = "user01"
+        let fileref = Storage.storage().reference().child("/\(tempUserID)/test")
+        let image = imageView.image!
+        //get the PNG data for this image
+        //        let data = image.pngData()
+        let imageData: Data = image.pngData()!
+        fileref.putData(imageData, metadata: meta, completion: { (meta, error) in
+            if error == nil {
+                self.ref2?.child("Usershaha/account2/image/test").setValue("filename")
+            }
+        })
+    }
+    
+    func randomString(length: Int) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        return String((0..<length).map{ _ in letters.randomElement()! })
+    }
+    
+    func saveImage(imageName: String){
+        //create an instance of the FileManager
+        let fileManager = FileManager.default
+        //get the image path
+        let imagePath = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(imageName)
+        //get the image we took with camera
+        let image = imageView.image!
+        //get the PNG data for this image
+        let data = image.pngData()
+        print(imagePath)
+        //store it in the document directory
+        fileManager.createFile(atPath: imagePath as String, contents: data, attributes: nil)
+    }
+    
+    @IBAction func savePhoto(_ sender: Any) {
+        //        let name = randomString(length: 10)
+        saveImage(imageName: "test")
+    }
+    func record() {
+        VideoHelper.startMediaBrowser(delegate: self, sourceType: .camera)
+    }
+    func display2(){
+        let actionSheetControllerIOS8: UIAlertController = UIAlertController(title: "Please select", message: "Option to select", preferredStyle: .actionSheet)
+        
+        let cancelActionButton = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            print("Cancel")
+        }
+        actionSheetControllerIOS8.addAction(cancelActionButton)
+        
+        let saveActionButton = UIAlertAction(title: "Take a photo", style: .default)
+        { _ in
+            self.takePhoto()
+            print("Take a photo")
+            self.choice = 1;
+        }
+        actionSheetControllerIOS8.addAction(saveActionButton)
+        
+        let deleteActionButton = UIAlertAction(title: "Record video", style: .default)
+        { _ in
+            self.choice = 2;
+            self.record()
+        }
+        actionSheetControllerIOS8.addAction(deleteActionButton)
+        self.present(actionSheetControllerIOS8, animated: true, completion: nil)
+    }
+    func displayActionSheet(){
+        // 1
+        let optionMenu = UIAlertController(title: nil, message: "Choose Option", preferredStyle: .actionSheet)
+        
+        // 2
+        let deleteAction = UIAlertAction(title: "Delete", style: .default)
+        let saveAction = UIAlertAction(title: "Save", style: .default)
+        
+        // 3
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        // 4
+        optionMenu.addAction(deleteAction)
+        optionMenu.addAction(saveAction)
+        optionMenu.addAction(cancelAction)
+        
+        // 5
+        self.present(optionMenu, animated: true, completion: nil)
+    }
+    
+    @objc func video(_ videoPath: String, didFinishSavingWithError error: Error?, contextInfo info: AnyObject) {
+        print("here")
+        let title = (error == nil) ? "Success" : "Error"
+        let message = (error == nil) ? "Video was saved" : "Video failed to save"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
